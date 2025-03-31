29808248e907ecd163f3a7896278a81fd85ccbcb
Daron <daronbaltazar@gmail.com>
2025-03-31 15:47:18 -0500

    MenuHandler Script
    MenuHandler Script
    Need audio in background for the cooler gotobutton method

---
+++ Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuHandler : MonoBehaviour
+{
+    public AudioSource source;
+
+ private IEnumerator WaitForSoundAndTransition(string sceneName){
+
+            source.Play();
+            yield return new WaitForSeconds(source.clip.length);
+            UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+
+    public void goToGame2(){
+        StartCoroutine(WaitForSoundAndTransition("Framework"));
+    }
+
+
+    public void goToMenu(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Main Menu");
+    }
+    public void goToGame(){
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Framework");
+    }
+}
