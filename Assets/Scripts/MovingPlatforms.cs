1a7da104b125cf8e56992999dfc810f37e020a48
Kate Spencer <katemspencer529@gmail.com>
2025-04-04 14:26:24 -0500

    finished moving platforms

--- Assets/Scripts/MovingPlatforms.cs
+++ Assets/Scripts/MovingPlatforms.cs
@@ -18,7 +18,10 @@ public class MovingPlatforms : MonoBehaviour
         if (Vector2.Distance(transform.position, points[i].position) < 0.02f)
         {
             i++;
-            //if (i)
+            if (i == points.Length) {
+                i = 0;
+            }
         }
+        transform.position = Vector2.MoveTowards(transform.position, points[i].position, speed * Time.deltaTime);
     }
 }
