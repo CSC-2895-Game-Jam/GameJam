e7cdba99015a03d332d9ceadd9139f71d425f5f5
Jake Berryman <dragons4me@comcast.net>
2025-04-04 15:43:18 -0500

    rework death zone to follow player x value, create death zone prefab

---
+++ Assets/Scripts/followPlayerX.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPlayerX : MonoBehaviour
+{
+    private GameObject player;
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    void FixedUpdate()
+    {
+        gameObject.transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+    }
+}
