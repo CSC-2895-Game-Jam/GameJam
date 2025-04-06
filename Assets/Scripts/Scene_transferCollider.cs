b921a743b96de45b54641d0b0cde75aa19813030
Layth-m <156365859+Layth-m@users.noreply.github.com>
2025-04-05 23:42:20 -0500

    Update scene transfer script, Add finishline Prefab.
    Added finish line prefab that when the player touches takes player to the next "level" or scene. The target scene number needs to assigned in the inspector for each instance of this prefab.

--- Assets/Scripts/Scene_transferCollider.cs
+++ Assets/Scripts/Scene_transferCollider.cs
@@ -5,7 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class Scene_transferCollider : MonoBehaviour
 {
-   public void LoadMenu() {
+
+    public int targetSceneIndex;
+
+    public string ActivateTag = "Player";
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag(ActivateTag))
+        {
+            SceneManager.LoadScene(targetSceneIndex);
+        }
+    }
+    public void LoadMenu() {
 
 
         SceneManager.LoadScene("MainMenu");
